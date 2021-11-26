a5e3f0357cfc491f87ae793ebf6a72ccaa6cb7ae
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2021-11-26 20:00:45 -0300

    Update site.js

--- site.js
+++ site.js
@@ -1,9 +1,22 @@
-var mysql = require('mysql')
+const express = require('express');
+const app = express()
+const mysql = require('mysql');
+
 var connection = mysql.createConnection({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
   password: '',
-  database: 'teste'
+  database: 'test'
 })
 
 connection.connect()
+
+connection.query("INSERT INTO test.user (nome, email, senha) VALUES ('teste1', 'teste1', 'teste1')", function(err,rows, fields){
+
+});
+
+connection.query('SELECT * FROM user', function(err,rows, fields){
+    console.log('resultado: ',rows)
+});
+
+connection.end()
